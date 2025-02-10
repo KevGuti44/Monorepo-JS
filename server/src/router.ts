cc2bf739a55b72cb97fe4f3da2dd04cc0bd5e6da
Kevin <kg71875@gmail.com>
2025-02-10 12:16:42 +0100

    MonoRepo quest 4 done

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Router } from "express";
 
 const router = express.Router();
 
@@ -7,9 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
+import categoryActions from "./modules/category/categoryActions";
 import itemActions from "./modules/item/itemActions";
 import programActions from "./modules/program/programActions";
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
